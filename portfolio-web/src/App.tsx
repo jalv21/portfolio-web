4ca0c9552c94879b0a2816270735b1f24db314f1
Jalv <jalv.rodrigues10@gmail.com>
2026-02-21 10:46:55 -0300

    feat: adicionando arquivos de imagem e wireframe da página inicial

--- portfolio-web/src/App.tsx
+++ portfolio-web/src/App.tsx
@@ -7,33 +7,12 @@ const person = {
 function App() {
   return (
     <>
-      <Profile />
+      
     </>
   );
 }
 
-export function Profile() {
-  let formattedName = person.occupation.replaceAll(' ', '');
-  return (
-    <>
-      <img
-        src={person.profilePicture}
-        alt="Developer's profile picture"
-        className="profile-img"
-      />
-      <h1>{person.name}</h1>
-      <p>{`<${formattedName} />`}</p>
-    </>
-  );
-}
 
-export function Menu() {
 
-  return (
-    <>
-    
-    </>
-  );
-}
 
 export default App;
