210e96d1a89059974fcb968bd3d92af31474b124
Jalv <jalv.rodrigues10@gmail.com>
2026-02-21 11:52:04 -0300

    feat: criando componente para seleção de linguagem

--- portfolio-web/src/components/LandingPage.tsx
+++ portfolio-web/src/components/LandingPage.tsx
@@ -1,34 +1,39 @@
 import React from 'react';
 
+let languages = [
+  { name: 'Portuguese', acronym: 'PT-BR', iconPath: '' }, 
+  { name: 'English', acronym: 'EN', iconPath: '' }
+];
+
 export default function PageLayout() {
   return (
-    <>
-        <LanguageMenu>
-            <LanguagePortuguese />
-            <LanguageEnglish />
-        </LanguageMenu>
-        <Profile>
-            <ProfilePicture />
-            <ProfileHeading />
-        </Profile>
-        <MainMenu />
-    </>
+    <section className="flex flex-col align-items-center justify-center h-screen">
+      <LanguageMenu>
+        {languages.map((language) => (<LanguageButton language={language} />))}
+      </LanguageMenu>
+      <Profile>
+        <ProfilePicture />
+        <ProfileHeading />
+      </Profile>
+      <MainMenu />
+    </section>
   );
 }
 
 export function LanguageMenu({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
-
-export function LanguagePortuguese() {
   return (
-    <></>
+    <div className="flex justify-center">
+      {children}
+    </div>
   );
 }
 
-export function LanguageEnglish() {
+export function LanguageButton({ language }: { language: any }) {
   return (
-    <></>
+    <button className="flex items-center justify-center m-2">
+      <img src={language.iconPath} alt={`${language.name} language icon`} className="mr-2" />
+      {language.acronym}
+    </button>
   );
 }
 
