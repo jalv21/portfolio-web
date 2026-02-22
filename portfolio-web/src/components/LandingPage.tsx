11b1d116264652128c100d6335ff26cb9fb228d5
Jalv <jalv.rodrigues10@gmail.com>
2026-02-22 11:00:38 -0300

    feat: criando componente de perfil

--- portfolio-web/src/components/LandingPage.tsx
+++ portfolio-web/src/components/LandingPage.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-let languages = [
+const languages = [
   { name: 'Portuguese', acronym: 'PT-BR', iconPath: './public/brazil-flag.png' },
   { name: 'English', acronym: 'EN', iconPath: './public/usa-flag.png' }
 ];
 
 export default function PageLayout() {
   return (
-    <section className="bg-black flex flex-col align-items-center justify-center h-screen">
+    <section className="bg-black flex flex-col items-center justify-around gap-2 h-screen">
       <LanguageMenu>
         {languages.map((language) => (<LanguageButton language={language} />))}
       </LanguageMenu>
@@ -30,7 +30,7 @@ export function LanguageMenu({ children }: { children: React.ReactNode }) {
 
 export function LanguageButton({ language }: { language: any }) {
   return (
-    <button className="bg-neutral-400 text-white font-bold font-mono flex items-center justify-center gap-2 px-4 py-2 rounded-full shadow shadow-neutral-600">
+    <button className="bg-neutral-400 text-white font-bold font-mono flex items-center justify-center gap-2 px-4 py-2 rounded-full shadow-lg shadow-neutral-500">
       <img src={language.iconPath} alt={`${language.name} language icon`} className="mr-2" />
       {language.acronym}
     </button>
@@ -39,21 +39,26 @@ export function LanguageButton({ language }: { language: any }) {
 
 export function Profile({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <section className="flex flex-col items-center justify-center">
       {children}
-    </>
+    </section>
   );
 }
 
 export function ProfilePicture() {
   return (
-    <></>
+    <div className="flex justify-center items-center h-48 w-48 rounded-full bg-linear-to-b from-purple-200 to-green-200">
+      <img src="./public/profile-picture.jpg" alt="Developer Profile Picture" className="size-9/10 object-fit rounded-full" />
+    </div>
   );
 }
 
 export function ProfileHeading() {
   return (
-    <></>
+    <div className="flex flex-col justify-center items-center gap-2 font-mono font-bold m-4 text-center">
+      <h1 className="text-5xl text-white">João Alvaro Rodrigues</h1>
+      <p className="text-2xl text-white">{'<'}<span className="text-green-200">SoftwareEngineer</span>{' />'}</p>
+    </div>
   );
 }
 
