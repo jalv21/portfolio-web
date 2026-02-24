2016a2597409abf3dbb64a36a2949c34fa62f2e8
João Álvaro Rodrigues de Araújo <1517742@sga.pucminas.br>
2026-02-24 09:09:03 -0300

    feat: update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,30 @@
 /**@type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: { extend: {} },
-    plugins: [],
-}
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        "float-fade": {
+          "0%, 100%": {
+            transform: "translateY(0) translateX(0)",
+            opacity: 0,
+          },
+          "25%": {
+            opacity: 0.6,
+          },
+          "50%": {
+            transform: "translateY(-100px) translateX(50px)",
+            opacity: 0.3,
+          },
+          "75%": {
+            opacity: 0.6,
+          },
+        },
+      },
+      animation: {
+        "float-fade": "float-fade linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
