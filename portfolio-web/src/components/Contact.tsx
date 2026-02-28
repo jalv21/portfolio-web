a74a7c5752e61ef427a8b3817a0c04e2b26367d7
Jalv <jalv.rodrigues10@gmail.com>
2026-02-28 19:12:53 -0300

    feat: estilização básica para novas páginas

--- portfolio-web/src/components/Contact.tsx
+++ portfolio-web/src/components/Contact.tsx
@@ -1,7 +1,13 @@
+import Background from "./Background";
+import NavigationBar from "./Navigation";
+
 export default function Contact() {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <h1 className="text-4xl font-bold text-white">Contato</h1>
-        </div>
+        <Background>
+            <NavigationBar innerMenu={[]} paths={[]} />
+            <div className="flex justify-center items-center h-screen font-jetbrains">
+                <h1 className="text-4xl font-bold text-white">Em breve</h1>
+            </div>
+        </Background>
     );
 }
--- portfolio-web/src/components/Experiences.tsx
+++ portfolio-web/src/components/Experiences.tsx
@@ -1,7 +1,13 @@
+import Background from "./Background";
+import NavigationBar from "./Navigation";
+
 export default function Experiences() {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <h1 className="text-4xl font-bold text-white">Experiências</h1>
-        </div>
+        <Background>
+            <NavigationBar innerMenu={[]} paths={[]} />
+            <div className="flex justify-center items-center h-screen font-jetbrains">
+                <h1 className="text-4xl font-bold text-white">Em breve</h1>
+            </div>
+        </Background>
     );
 }
